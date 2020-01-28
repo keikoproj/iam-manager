33ad52463a064d1eaf7e2b94116f30649eed67d5
kshamajain99 <kshamajain99@gmail.com>
2020-01-28 11:45:01 -0800

    attach managed policy to role

--- internal/config/properties.go
+++ internal/config/properties.go
@@ -2,16 +2,18 @@ package config
 
 import (
 	"context"
-	"github.com/keikoproj/iam-manager/internal/k8s"
 	"strings"
+
+	"github.com/keikoproj/iam-manager/internal/k8s"
 )
 
 const (
 	iamPolicyWhitelist    = "iam.policy.action.prefix.whitelist"
 	iamPolicyBlacklist    = "iam.policy.resource.blacklist"
 	iamPolicyS3Restricted = "iam.policy.s3.restricted.resource"
-	awsAccountId = "aws.accountId"
-	awsMasterRole = "aws.MasterRole"
+	awsAccountId          = "aws.accountId"
+	awsMasterRole         = "aws.MasterRole"
+	managedPolicies       = "iam.managed.policies"
 )
 
 const (
@@ -23,8 +25,9 @@ type Properties struct {
 	AllowedPolicyAction       []string
 	RestrictedPolicyResources []string
 	RestrictedS3Resources     []string
-	AWSAccountId string
-	AWSMasterRole string
+	AWSAccountId              string
+	AWSMasterRole             string
+	ManagedPolicies           []string
 }
 
 //LoadProperties function loads properties from various sources
@@ -33,6 +36,7 @@ func LoadProperties(ctx context.Context, kClient *k8s.Client, ns string, cmName 
 	allowedActions := strings.Split(res.Data[iamPolicyWhitelist], separator)
 	restrictedResources := strings.Split(res.Data[iamPolicyBlacklist], separator)
 	restrictedS3Resources := strings.Split(res.Data[iamPolicyS3Restricted], separator)
+	managedPolicies := strings.Split(res.Data[managedPolicies], separator)
 	awsAccountId := res.Data[awsAccountId]
 	awsMasterRole := res.Data[awsMasterRole]
 
@@ -40,8 +44,9 @@ func LoadProperties(ctx context.Context, kClient *k8s.Client, ns string, cmName 
 		AllowedPolicyAction:       allowedActions,
 		RestrictedPolicyResources: restrictedResources,
 		RestrictedS3Resources:     restrictedS3Resources,
-		AWSAccountId: awsAccountId,
-		AWSMasterRole: awsMasterRole,
+		AWSAccountId:              awsAccountId,
+		AWSMasterRole:             awsMasterRole,
+		ManagedPolicies:           managedPolicies,
 	}
 
 }

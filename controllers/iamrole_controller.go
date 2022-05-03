e8c1661c4a10258cd7c22ad57b1f4d7a7f37e9a0
Delyan Raychev <delyanr+github@gmail.com>
2022-05-02 20:51:45 -0700

    Simplify the usage of GetServiceAccount
    Signed-off-by: Delyan Raychev <delyanr+github@gmail.com>

--- controllers/iamrole_controller.go
+++ controllers/iamrole_controller.go
@@ -191,10 +191,8 @@ func (r *IamroleReconciler) HandleReconcile(ctx context.Context, req ctrl.Reques
 		saConsistent := false
 		saExists, saName := utils.ParseIRSAAnnotation(ctx, iamRole)
 		if saExists {
-			// Get the service account in kubernetes
-			saSpec := k8s.NewK8sManagerClient(r.Client).GetServiceAccount(ctx, iamRole.Namespace, saName)
 			// If it exists, check the annotations are correct
-			if saSpec != nil {
+			if saSpec := k8s.NewK8sManagerClient(r.Client).GetServiceAccount(ctx, iamRole.Namespace, saName); saSpec != nil {
 				saConsistent = validation.CompareRoleIRSA(ctx, saSpec, *config.Props)
 			}
 		}
